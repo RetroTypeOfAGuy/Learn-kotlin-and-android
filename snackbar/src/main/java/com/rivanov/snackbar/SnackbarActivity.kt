ac497481cd529515bab72ae5bae24b2e103e4e2b
Racho Ivanov <racho_ivanov@abv.bg>
2022-04-04 20:37:25 +0300

    Snackbar exploration

--- snackbar/src/main/java/com/rivanov/snackbar/SnackbarActivity.kt
+++ snackbar/src/main/java/com/rivanov/snackbar/SnackbarActivity.kt
@@ -1,11 +1,42 @@
 package com.rivanov.snackbar
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.snackbar.Snackbar
+import kotlin.random.Random
 
 class SnackbarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_snackbar)
+
+        val snackButton = findViewById<Button>(R.id.snack_me_button)
+        val snackLayout = findViewById<CoordinatorLayout>(R.id.snackbar_container)
+
+        val snack = Snackbar.make(snackLayout, "tasty", Snackbar.LENGTH_INDEFINITE);
+        snack.setAction("Yes please!") {
+            snack.dismiss()
+        }
+
+        snack.setBackgroundTint(Color.BLACK)
+        val snacksList = ArrayList<String>()
+        snacksList.add("Banana")
+        snacksList.add("Chips")
+        snacksList.add("Waffle")
+
+        val random = Random(1)
+        snack.setTextColor(Color.GREEN)
+
+
+
+        snackButton.setOnClickListener {
+            snack.setText("Care for a tasty " + snacksList.get(random.nextInt(snacksList.size)))
+            snack.show()
+        }
     }
 }
